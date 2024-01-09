fc5b87e58e61478fa99ceb1931c82c1e747f1eb6
Sohil Khan <zenon.byte@gmail.com>
2024-01-10 00:59:49 +0530

    feat: add selected project component

---
+++ src/components/NewTask.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function NewTask({ onAdd }) {
+    const [inputText, setInputText] = React.useState("");
+
+    const handleChange = (event) => {
+        setInputText(event.target.value);
+    };
+    const handleClick = () => {
+        if (inputText.trim() === "") {
+            return;
+        }
+        onAdd(inputText);
+        setInputText("");
+    };
+    return (
+        <div className="flex items-center gap-4">
+            <input
+                onChange={handleChange}
+                value={inputText}
+                type="text"
+                className="w-64 px-2 py-1 rounded-sm bg-stone-200"
+            />
+            <button
+                onClick={handleClick}
+                className="text-stone-700 hover:text-stone-950"
+            >
+                Add Task
+            </button>
+        </div>
+    );
+}
